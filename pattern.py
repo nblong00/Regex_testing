0799122fe7bf43d510c5b685a2575c63a567d9a1
Blaise <blazzze0517@gmail.com>
2025-06-11 16:47:56 -0400

    Made phone numbers different in sample. Cleaned up pattern.py. Same functionality, less code.

--- pattern.py
+++ pattern.py
@@ -1,14 +1,11 @@
 import re
 
-samplefile = open('sample.txt', encoding='utf-8')
-data = samplefile.read()
-samplefile.close()
-print(data)
-pattern = re.compile(r'1234')
+pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
 
-matches = pattern.finditer(data)
+with open('sample.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
+    matches = pattern.finditer(data)
+    print(data)
 
-for match in matches:
-    print(match)
-
-print(data[161:169])
+    for match in matches:
+        print(match)
