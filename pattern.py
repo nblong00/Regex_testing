d800e8dd19f7d57b893cd44063cd9b8f4bc504a6
Blaise <blazzze0517@gmail.com>
2025-06-12 16:51:41 -0400

    Now using expect values in {} regex pattern.

--- pattern.py
+++ pattern.py
@@ -1,11 +1,11 @@
 import re
 
-pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+pattern = re.compile(r'\d{3}[-\s.]\d{3}[-\s.]\d{4}')
 
 with open('sample.txt', 'r', encoding='utf-8') as file:
     data = file.read()
     matches = pattern.finditer(data)
-    print(data)
+    # print(data)
 
     for match in matches:
         print(match)
