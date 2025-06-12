37b5bd5c6061b12977d2f590f1d0e87058b1afcf
Blaise <blazzze0517@gmail.com>
2025-06-11 21:04:17 -0400

    initial push of script which can check user input to see if it conforms to typical email format.

---
+++ emailchecker.py
@@ -0,0 +1,17 @@
+import re
+import time
+
+email_pattern = re.compile(r'''
+                           ^[-\w\d.+]+@[-\w\d+]+\.[\w\d-]+$
+                           ''', re.X)
+
+user_email = input("Enter an email to check> ")
+
+if email_pattern.match(user_email):
+    print("Correct email format used.\nClosing...")
+    time.sleep(2)
+    exit()
+else:
+    print("That email doesn't match current email format expectations\nClosing...")
+    time.sleep(2)
+    exit()
