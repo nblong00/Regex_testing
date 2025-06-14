251e634bd231c436a23af568406848d6a58597f4
Blaise <blazzze0517@gmail.com>
2025-06-13 20:09:11 -0400

    Pushed code to separate functions, and add main(). Reformatted code.

--- emailchecker.py
+++ emailchecker.py
@@ -1,24 +1,39 @@
 import re
 import time
 
-email_pattern = re.compile(r'''
-                           ^[-\w.+]+@[-\w+]+\.[-\w]+\.?
-                           [-\w]+?\.?[-\w]+?$
-                           ''', re.X)
-user_email = input("Enter an email to check:\n> ")
-while 1 > 0:
-    if email_pattern.match(user_email):
-        print("Correct email format used.\n")
-    else:
-        print("That email doesn't match current email format expectations.\n")
-    run_again = input("Want to check another email (yes/no):\n> ")
-    if run_again in ["n", "no"]:
-        print("Program closing...")
-        time.sleep(1)
-        exit()
-    elif run_again in ["yes", "ye", "y"]:
-        user_email = input("Enter new email to check:\n> ")
-    elif run_again not in ["yes", "ye", "y", "n", "no"]:
-        print("Invalid input.\nClosing....")
-        time.sleep(1)
-        exit()
+
+def email_pattern():
+    email_pattern = re.compile(r'''
+                               ^[-\w.+]+@[-\w+]+\.[-\w]+\.?
+                               [-\w]+?\.?[-\w]+?$
+                               ''', re.X)
+    return email_pattern
+
+
+def primary_logic_loop(email_pattern):
+    user_email = input("Enter an email to check:\n> ")
+    while 1 > 0:
+        if email_pattern.match(user_email):
+            print("Correct email format used.\n")
+        else:
+            print("That email doesn't match current email format expectations.\n")
+        run_again = input("Want to check another email (yes/no):\n> ")
+        if run_again in ["n", "no"]:
+            print("Program closing...")
+            time.sleep(1)
+            exit()
+        elif run_again in ["yes", "ye", "y"]:
+            user_email = input("Enter new email to check:\n> ")
+        elif run_again not in ["yes", "ye", "y", "n", "no"]:
+            print("Invalid input.\nClosing....")
+            time.sleep(1)
+            exit()
+
+
+def main():
+    email_pattern = email_pattern()
+    primary_logic_loop(email_pattern)
+
+
+if __name__ == '__main__':
+    main()
